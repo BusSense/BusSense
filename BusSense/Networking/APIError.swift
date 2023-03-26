9a74a7b37082088d2c01864f6fd9c75bf4371fb1
daniel-a-r <daniel.a-r@outlook.com>
2023-03-26 01:24:17 -0400

    return more descriptive errors

--- BusSense/Networking/APIError.swift
+++ BusSense/Networking/APIError.swift
@@ -32,9 +32,11 @@ enum APIError: Error, CustomStringConvertible {
         case .unknown: return "unknown error"
         case .badURL: return "invalid URL"
         case .url(let error):
-            return error?.localizedDescription ?? "url session error"
+            return "url session error - \(String(describing: error))"
+//            return error?.localizedDescription ?? "url session error"
         case .parsing(let error):
-            return "parsing error \(error?.localizedDescription ?? "")"
+            return "parsing error - \(String(describing: error))"
+//            return "parsing error \(error?.localizedDescription ?? "")"
         case .badResponse(statusCode: let statusCode):
             return "bad response with status code \(statusCode)"
         }
