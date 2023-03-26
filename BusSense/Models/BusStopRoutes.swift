5e74247bc45ae5c5940297171f327e22b8341f09
daniel-a-r <daniel.a-r@outlook.com>
2023-03-25 20:41:53 -0400

    Add BusStopRoutes.swift model

--- BusSense/Models/BusStopRoutes.swift
+++ BusSense/Models/BusStopRoutes.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct BusStopRoutes {
+    let code: String
+    let id: String
+    let name: String
+    let direction: String
+    let lat: Double
+    let lon: Double
+    let locationType: Int
+    let routes: [RouteDetail]
+    let wheelchairBoarding: String
+}
+
+struct RouteDetail {
+    let lineRef: String
+    let directionRef: String
+    let journeyPatternRef: String
+    let publishedLineName: String
+    let operatorRef: String
+    let originRef: String
+    let destinationRef: String
+    let destinationName: String
+}
