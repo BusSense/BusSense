8b1293c6f1db5b26d764ef6f2d02b430c3762e2d
daniel-a-r <daniel.a-r@outlook.com>
2023-05-09 02:02:09 -0400

    Add icon and message to default view

--- BusSense/Views/DefaultView.swift
+++ BusSense/Views/DefaultView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct DefaultView: View {
     var body: some View {
-        Text("This is the default view for when the user opens up the app for the first time and permissions are requested")
+        ZStack {
+            Color("Color1").ignoresSafeArea()
+            
+            VStack {
+                Image(systemName: "bus.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Text("Please enable location services to enable bus tracking!")
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
+        }
     }
 }
 
