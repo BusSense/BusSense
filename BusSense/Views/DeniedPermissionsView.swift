8ef48089eaec215fe879f2fd4416d58c34aeefad
daniel-a-r <daniel.a-r@outlook.com>
2023-05-09 01:51:23 -0400

    Add icon and message for denied location permission

--- BusSense/Views/DeniedPermissionsView.swift
+++ BusSense/Views/DeniedPermissionsView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct DeniedPermissionsView: View {
     var body: some View {
-        Text("This shows the denied permissions that are required for the app to function properly")
+        ZStack {
+            Color("Color1").ignoresSafeArea()
+            
+            VStack {
+                Image(systemName: "location.slash.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Text("Location services have been disabled.").padding()
+            }
+        }
     }
 }
 
